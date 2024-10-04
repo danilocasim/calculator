8e5b9fb695ac4fadecb06b9559ab864f6da5ace4
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-10-04 09:50:57 +0800

    Add func operate that takes 2 num and operator

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+      break;
+    case "-":
+      return subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      return multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      return divide(firstNumber, secondNumber);
+      break;
+  }
+}
