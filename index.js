9733ab9a83f2baa341ddfd12b689ad62bf42e3ca
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-10-05 15:23:44 +0800

    Add add,subtract,multiply,divide and operate function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const liveView = document.querySelector(".live-solution");
+const buttons = document.querySelectorAll("button");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -15,10 +20,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
-
-let firstNumber;
-let operator;
-let secondNumber;
 
 function operate(firstNumber, operator, secondNumber) {
   switch (operator) {
